0a4078d65ea2aa5ddef7c5c6da216654e41134c6
ivano panizza <ivano.panizza@libero.it>
2019-04-12 10:28:12 +0200

    pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'my_works', to: 'pages#my_works', as: :my_works
+
+  root to: 'pages#home'
+  # get 'pages/contact'
+  # get 'pages/about'
+  # get 'pages/my_works'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

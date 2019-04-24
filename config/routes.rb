7a4019e49be1c3bf7a00e73c77efcb4cd5dd42fc
ivano panizza <ivano.panizza@libero.it>
2019-04-24 16:03:21 +0200

    contacts backend, still need to fix routes for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
-  get 'contact', to: 'pages#contact', as: :contact
   get 'my_works', to: 'pages#my_works', as: :my_works
+  get 'contact', to: 'pages#contact', as: :contact
+
+  resources "contacts", only: [:new, :create]
 
   root to: 'pages#home'
   # get 'pages/contact'

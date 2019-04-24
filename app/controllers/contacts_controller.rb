5c41e0c3e7ef30501917d0595d1de309376037a2
ivano panizza <ivano.panizza@libero.it>
2019-04-24 22:34:29 +0200

    some style added to pages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,8 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      # flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      render :create
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'my_works', to: 'pages#my_works', as: :my_works
-  get 'contact', to: 'pages#contact', as: :contact
 
   resources "contacts", only: [:new, :create]
+  get 'contacts', to: 'contacts#new', as: :contact
 
   root to: 'pages#home'
   # get 'pages/contact'
